ac712af0bda399cce9eeca7723f44726d09a03b2
hermas05 <130081754+hermas05@users.noreply.github.com>
2024-07-06 19:23:09 +0530

    Add files via upload

---
+++ src/Components/menuList.jsx
@@ -0,0 +1,234 @@
+import lasa from '../assets/background.jpg'
+import pica from '../assets/pica.jpg'
+import piza from '../assets/piza.jpg'
+import crap from '../assets/crap.jpg'
+import cick from '../assets/cick.jpg'
+import drink from '../assets/drink.jpg'
+import bur from '../assets/bur.jpg'
+import bun from '../assets/bun.jpg'
+
+const id = Date.now().toString(36) + Math.random().toString(36);
+
+export const MenuList =[
+
+    {
+        id : "1",
+        image:crap,
+        name : "Italian Tortellini Pasta Salad",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 50,
+        type : "drink",
+        count : 1 
+    },
+    {
+        id : "2",
+        image:bun,
+        name : "Aperol Spritz ",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 40,
+        type : "drink",
+        count : 1 
+    },
+    
+    
+    {
+        id : "3",
+        image:cick,
+        name : "Pollo alla Cacciatora",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 490,
+        type : "main",
+        count : 1 
+    },
+    {
+        id:"4",
+        image:bur,
+        name : "Pizza Margherita",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 300,
+        type : "main",
+        count : 1 
+    },
+    {
+        id:"5",
+        image:drink,
+        name : "Costoletta alla Milanese",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 90,
+        type : "main",
+        count : 1 
+    },
+    {
+        id:"6",
+        image:bun,
+        name : "Vitello Tonnato",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 40,
+        type : "main",
+        count : 1 
+    },
+    {
+        id:"7",
+        image:bur,
+        name : "Caprese Salad",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 50,
+        type : "salad",
+        count : 1 
+    },
+    {
+        id:"8",
+        image:pica,
+        name : "Panzanella",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 670,
+        type : "salad",
+        count : 1 
+    },
+    {
+        id:"9",
+        image:bun,
+        name : "Italian Octopus Salad",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 75,
+        type : "salad",
+        count : 1 
+    },
+    {
+        id:"10",
+        image:lasa,
+        name : "Burger",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 50,
+        type : "salad",
+        count : 1 
+    },
+    {
+        id:"11",
+        image:drink,
+        name : "Italian Rice Salad",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 120,
+        type : "salad",
+        count : 1 
+    },
+    {
+        id:"13",
+        image:cick,
+        name : "Italian Antipasto Salad",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 230,
+        type : "salad",
+        count : 1 
+    },
+    {
+        id:"14",
+        image:piza,
+        name : "Italian Salad with 3 Lettuces",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 750,
+        type : "salad",
+        count : 1 
+    },
+    {
+        id:"15",
+        image:drink,
+        name : "Italian Sub Broccoli Salad",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 80,
+        type : "salad",
+        count : 1 
+    },
+    
+    {
+        id:"16",
+        image:bur,
+        name : "Amaretto Liqueur",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 900,
+        type : "drink",
+        count : 1 
+    },
+    {
+        id:"17",
+        image:lasa,
+        name : "Campari ",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 865,
+        type : "drink",
+        count : 1 
+    },
+    {
+        id:"18",
+        image:crap,
+        name : "Pinot Grigio",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 950,
+        type : "drink",
+        count : 1 
+    },
+    {
+        id:"19",
+        image:drink,
+        name : "Wine Chianti",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 40,
+        type : "drink",
+        count : 1 
+    },
+    {
+        id:"20",
+        image:piza,
+        name : "Barolo",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 670,
+        type : "drink",
+        count : 1 
+    },
+    {
+        id:"21",
+        image:lasa,
+        name : "Lasagne alla Bolognese",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 450,
+        type : "main",
+        count : 1 
+    },
+    {
+        id:"22",
+        image:pica,
+        name : "Fettuccine al Pomodoro",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 40,
+        type : "main",
+        count : 1 
+    },
+    {
+        id:"23",
+        image:piza,
+        name : "Gnocchi di Patate",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 560,
+        type : "main",
+        count : 1 
+    },
+    {
+        id:"24",
+        image:crap,
+        name : "Melanzane alla Parmigiana",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 250,
+        type : "main",
+        count : 1 
+    },
+    {
+        id: "25",
+        image:pica,
+        name : "Limoncello",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        price : 45,
+        type : "drink",
+        count : 1 
+    },
+    
+];
